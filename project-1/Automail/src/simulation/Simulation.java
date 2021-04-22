ac69e41a4c4b28d76817dc03cf72f9134645059b
Lucas Fern <lucaslfern@gmail.com>
2021-04-22 12:04:17 +1000

    finalised for submission

--- project-1/Automail/src/simulation/Simulation.java
+++ project-1/Automail/src/simulation/Simulation.java
@@ -114,7 +114,7 @@ public class Simulation {
     	automailProperties.setProperty("Floors", "10");
     	automailProperties.setProperty("Mail_to_Create", "80");
     	automailProperties.setProperty("ChargeThreshold", "0");
-    	automailProperties.setProperty("ChargeDisplay", "true");
+    	automailProperties.setProperty("ChargeDisplay", "false");
     	
     	// Read properties
 		FileReader inStream = null;
